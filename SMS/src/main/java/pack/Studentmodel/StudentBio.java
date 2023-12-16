ac0157518f035240352bbb59776a81516a5182b4
R.O.O.T.S <152678765+Rootsinc23@users.noreply.github.com>
2023-12-15 20:22:52 -0500

    Update StudentBio.java

--- SMS/src/main/java/pack/Studentmodel/StudentBio.java
+++ SMS/src/main/java/pack/Studentmodel/StudentBio.java
@@ -27,5 +27,81 @@ public class StudentBio {
 	@Column(name = "religion")
 	protected String religion;
 	
-	// Student Bio class without arguments
+	// StudentBio class without arguments
+	public StudentBio() {
+		
+	}
+	
+	// StudentBio class with arguments
+	public StudentBio(int id, String surname, String firstName, boolean gender, String bloodGroup, String nationality, String religion) {
+		super();
+		
+		this.id = id;
+		this.surname = surname;
+		this.firstName = firstName;
+		this.gender = gender;
+		this.bloodGroup = bloodGroup;
+		this.nationality = nationality;
+		this.religion = religion;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getSurname() {
+		return surname;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public boolean isGender() {
+		return gender;
+	}
+
+	public void setGender(boolean gender) {
+		this.gender = gender;
+	}
+
+	public String getBloodGroup() {
+		return bloodGroup;
+	}
+
+	public void setBloodGroup(String bloodGroup) {
+		this.bloodGroup = bloodGroup;
+	}
+
+	public String getNationality() {
+		return nationality;
+	}
+
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+
+	public String getReligion() {
+		return religion;
+	}
+
+	public void setReligion(String religion) {
+		this.religion = religion;
+	}
+	
+	
+	
+	
+	
 }
